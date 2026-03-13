702e0191ee7cc3567ea3266ddf4fc9874ad22d6e
aovando <aovandodevsxr@gmail.com>
2026-03-13 15:30:30 -0400

    character and text change

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,8 +1,17 @@
+using com.rfilkov.components;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
+    private SceneGestureListener gestureListener;
+
+    private void Start()
+    {
+        // get the gestures listener
+        gestureListener = SceneGestureListener.Instance;
+    }
+
     private void Update()
     {
         // Check if L key is pressed
@@ -10,11 +19,16 @@ public class SceneTransition : MonoBehaviour
         {
             LoadNextScene();
         }
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        LoadNextScene();
+        // Don't run if there is no gesture listener
+        if (!gestureListener)
+            return;
+
+        // Check for swipe right gesture
+        if (gestureListener.IsSwipeRight())
+        {
+            LoadNextScene();
+        }
     }
 
     private void LoadNextScene()
