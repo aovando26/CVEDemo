f4f95a979cd1e3ece8e693c01c53d72a96f9acd5
aovando <aovandodevsxr@gmail.com>
2026-03-14 23:23:57 -0400

    lower letter mat color change

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,11 +1,16 @@
 using com.rfilkov.components;
 using com.rfilkov.kinect;
+//using System.Collections;
+//using Unity.VectorGraphics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using UnityEngine.UI;
 
 public class SceneTransition : MonoBehaviour
 {
     private SceneGestureListener gestureListener;
+    //public Image image; 
+    
 
     private void Awake()
     { 
@@ -13,6 +18,11 @@ public class SceneTransition : MonoBehaviour
 
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
+
+    //private void Start()
+    //{
+    //    FadeOut();
+    //}
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
@@ -38,6 +48,7 @@ public class SceneTransition : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.L))
         {
+            //FadeAndLoad("GameScene", 1);
             LoadNextScene();
         }
 
@@ -48,6 +59,7 @@ public class SceneTransition : MonoBehaviour
 
         if (gestureListener.IsSwipeUp())
         {
+            //FadeAndLoad("GameScene", 1);
             LoadNextScene();
         }
     }
@@ -57,9 +69,42 @@ public class SceneTransition : MonoBehaviour
         Debug.Log($"Current Scene: {SceneManager.GetActiveScene().name}");
         SceneManager.LoadScene(1);
     }
-
     private void OnDestroy()
     {
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
+
+    //public void FadeAndLoad(string sceneName, float duration)
+    //{ 
+    //    StartCoroutine(Fader(sceneName, duration));
+    //}
+
+    //IEnumerator Fader(string sceneName, float duration)
+    //{
+    //    float t = 0;
+    //    Color c = image.color;
+
+    //    while (t < duration)
+    //    { 
+    //        t += Time.deltaTime;
+    //        c.a = t / duration;
+    //        yield return null;
+    //    }
+
+    //    SceneManager.LoadScene(sceneName);
+    //}
+
+    //IEnumerator FadeOut()
+    //{
+    //    float t = 0;
+    //    Color c = image.color;
+
+    //    while (t < 1)
+    //    {
+    //        t += Time.deltaTime;
+    //        c.a = 1f - (t / 1f);
+    //        image.color = c;
+    //        yield return null;
+    //    }
+    //}
 }
